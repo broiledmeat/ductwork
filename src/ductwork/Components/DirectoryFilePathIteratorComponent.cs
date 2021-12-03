b825febf14f1e89557b5a4a958e98e38957421ab
Derrick Staples <broiledmeat@gmail.com>
2021-12-02 22:16:46 -0500

    Support not iterating on hidden paths in the DirectoryFilePathIterator

--- src/ductwork/Components/DirectoryFilePathIteratorComponent.cs
+++ src/ductwork/Components/DirectoryFilePathIteratorComponent.cs
@@ -10,11 +10,13 @@ public class DirectoryFilePathIteratorComponent : SingleOutComponent<FilePathArt
 {
     public readonly string Path;
     public readonly bool IsRecursive;
+    public readonly bool IncludeHidden;
 
-    public DirectoryFilePathIteratorComponent(string path, bool isRecursive = true)
+    public DirectoryFilePathIteratorComponent(string path, bool isRecursive = true, bool includeHidden = false)
     {
-        Path = path;
+        Path = System.IO.Path.GetFullPath(path);
         IsRecursive = isRecursive;
+        IncludeHidden = includeHidden;
     }
 
     public override async Task Execute(Graph graph, CancellationToken token)
@@ -24,8 +26,36 @@ public class DirectoryFilePathIteratorComponent : SingleOutComponent<FilePathArt
         foreach (var path in Directory.EnumerateFiles(Path, "*.*", options))
         {
             token.ThrowIfCancellationRequested();
+
+            if (!IncludeHidden && IsPathOrParentsHidden(path))
+            {
+                continue;
+            }
+
             var artifact = new FilePathArtifact(path);
             await graph.Push(Out, artifact);
         }
+    }
+
+    private bool IsPathOrParentsHidden(string path)
+    {
+        while (!System.IO.Path.GetFullPath(path).Equals(Path))
+        {
+            if (File.GetAttributes(path).HasFlag(FileAttributes.Hidden))
+            {
+                return true;
+            }
+
+            if (System.IO.Path.GetDirectoryName(path) is { } parentPath)
+            {
+                path = parentPath;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return false;
     }
 }
