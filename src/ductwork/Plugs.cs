d3c4db79ef059546f85d58e209938ea28861339e
Derrick Staples <broiledmeat@gmail.com>
2023-05-01 21:56:18 -0400

    Add an IPlug interface and have Input and OutputPlug implement it. Fix doc strings.

--- src/ductwork/Plugs.cs
+++ src/ductwork/Plugs.cs
@@ -1,19 +1,23 @@
 #nullable enable
 namespace ductwork;
 
+public interface IPlug
+{
+}
+
 /// <summary>
-/// Data input plug for <see cref="Components.Component"/>. Used by <see cref="GraphBuilder"/> to connect data flow in
-/// from a <see cref="OutputPlug"/> on another Component.
+/// Data input plug for <see cref="Components.Component"/>. Used by <see cref="Executors.IExecutor"/> to connect data
+/// flow in from a <see cref="OutputPlug"/> on another Component.
 /// </summary>
-public class InputPlug
+public class InputPlug : IPlug
 {
 }
 
 
 /// <summary>
-/// Data output plug for <see cref="Components.Component"/>. Used by <see cref="GraphBuilder"/> to connect data flow out
-/// from a Component to an <see cref="InputPlug"/> on another Component.
+/// Data output plug for <see cref="Components.Component"/>. Used by <see cref="Executors.IExecutor"/> to connect data
+/// flow out from a Component to an <see cref="InputPlug"/> on another Component.
 /// </summary>
-public class OutputPlug
+public class OutputPlug : IPlug
 {
 }
